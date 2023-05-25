30b6803cd78fa2230fff6ae1fc484ff590929acb
Git290234 <viktorkuenstler@googlemail.com>
2023-05-25 20:59:56 +0200

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+console.log('JavaScript-Datei wurde erfolgreich eingebunden.');
